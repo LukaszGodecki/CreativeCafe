6ff4f83418bf8569caab6036a5cda3eea95ef194
LukaszGodecki <34724211+LukaszGodecki@users.noreply.github.com>
2018-12-28 22:42:39 +0100

    Feature/navi pages components (#3)
    * Navi Pages Components
    
    * Navi Pages Components Add

--- CreativeCafe/src/app/app-routing.module.ts
+++ CreativeCafe/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: MainComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "**", redirectTo: "", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
